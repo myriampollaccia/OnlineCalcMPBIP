a6759602f57950a600df855dc560a154671a1b1b
Myriam P <myriampollaccia@gmail.com>
2024-07-16 16:11:45 +0200

    Conflict risolti

--- test_api.py
+++ test_api.py
@@ -18,18 +18,28 @@ class TestCalculatorAPI(unittest.TestCase):
         print("subtract ok")
 
     def test_multiply(self):
+        response = self.client.get("/multiply?a=20&b=10")
+        self.assertEqual(response.json["result"], 200)
         print("multiply ok")
 
     def test_divide(self):
+        response = self.client.get("/divide?a=20&b=10")
+        self.assertEqual(response.json["result"], 2)
         print("divide ok")
 
     def test_power(self):
+        response = self.client.get("/power?a=20&b=10")
+        self.assertEqual(response.json["result"], 10240000000000)
         print("power ok")
 
     def test_square_root(self):
+        response = self.client.get("/square_root_num?a=9")
+        self.assertEqual(response.json["result"], 3)
         print("square root ok")
 
     def test_modulus(self):
+        response = self.client.get("/modulus?a=20&b=10")
+        self.assertEqual(response.json["result"], 0)
         print("modulus ok")
 
 
